3ee0bb623f37e67eb46deb06a91b6e95d196bec0
Varun-xd <60666393+Varun-xd@users.noreply.github.com>
2024-07-22 15:57:23 +0530

    Update Workout.jsx

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -5,16 +5,18 @@ import ExerciseCard from "./ExerciseCard";
 export default function Workout(props) {
   const { workout } = props;
   return (
-    <SectionWrapper
-      id={"workout"}
-      header={"welcome to"}
-      title={["The", "DANGER", "Zone"]}
-    >
-      <div className="flex flex-col gap-4">
-        {workout.map((exercise, i) => {
-          return <ExerciseCard i={i} exercise={exercise} key={i} />;
-        })}
-      </div>
-    </SectionWrapper>
+    <div className="mt-10">
+      <SectionWrapper
+        id={"workout"}
+        header={"welcome to"}
+        title={["The", "DANGER", "Zone"]}
+      >
+        <div className="flex flex-col gap-4">
+          {workout.map((exercise, i) => {
+            return <ExerciseCard i={i} exercise={exercise} key={i} />;
+          })}
+        </div>
+      </SectionWrapper>
+    </div>
   );
 }
